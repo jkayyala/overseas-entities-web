d79c90825f8d23863407a88de30510bbd8b0d255
jkayyala <jkayyala@companieshouse.gov.uk>
2022-11-24 11:56:14 +0000

    ROE Update Landing page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,6 +15,8 @@ import {
   healthcheck,
   interruptCard,
   landing,
+  updateLanding,
+  overseasEntitySearch,
   managingOfficerIndividual,
   managingOfficerCorporate,
   presenter,
@@ -44,12 +46,15 @@ router.get(config.HEALTHCHECK_URL, healthcheck.get);
 router.get(config.ACCESSIBILITY_STATEMENT_URL, accessibilityStatement.get);
 
 router.get(config.LANDING_URL, landing.get);
+router.get(config.UPDATE_LANDING_URL, updateLanding.get);
 
 router.get(config.SIGN_OUT_URL, signOut.get);
 router.post(config.SIGN_OUT_URL, ...validator.signOut, checkValidations, signOut.post);
 
 router.get(config.SOLD_LAND_FILTER_URL, authentication, soldLandFilter.get);
 router.post(config.SOLD_LAND_FILTER_URL, authentication, ...validator.soldLandFilter, checkValidations, soldLandFilter.post);
+
+router.get(config.OVERSEAS_ENTITY_SEARCH_URL, authentication, overseasEntitySearch.get);
 
 router.get(config.CANNOT_USE_URL, authentication, cannotUse.get);
 
